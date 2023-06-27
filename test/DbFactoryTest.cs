8f85b1322f0cf27a6b65749cdedc74539f703381
Oliver Rosas Gonzalez <oliverrosasg@gmail.com>
2023-06-27 13:03:00 -0600

    Apply last comments

--- test/DbFactoryTest.cs
+++ test/DbFactoryTest.cs
@@ -1,69 +1,71 @@
-using DbConnectionFactory;
+﻿using DbConnectionFactory;
 using DbConnectionFactory.Adapters;
 using DbConnectionFactory.Enums;
 using Microsoft.Extensions.Configuration;
 using Moq;
 
-namespace DbConnectionFactoryTest;
-
-public class DbFactoryTest
+namespace DbConnectionFactoryTests
 {
-    private readonly DbFactory _factory;
-    
-    public DbFactoryTest()
+
+    public class DbFactoryTest
     {
-        _factory = new ();
-    }
-    
-    [Fact]
-    public void CreateConnection_ServerTypeSQL_ReturnConnectionSQLServer()
-    {
-        var serverType = ServerType.SqlServer;
+        private readonly DbFactory _factory;
 
-        var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
+        public DbFactoryTest()
+        {
+            _factory = new();
+        }
 
-        Assert.NotNull(result);
-        Assert.IsType<SqlServerAdapter>(result);
-    }
-    
-    [Fact]
-    public void CreateConnection_ServerTypeMySql_ReturnConnectionMySql()
-    {
-        var serverType = ServerType.MySql;
+        [Fact]
+        public void CreateConnection_ServerTypeSQL_ReturnConnectionSQLServer()
+        {
+            var serverType = ServerType.SqlServer;
 
-        var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
+            var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
 
-        Assert.NotNull(result);
-        Assert.IsType<MySqlAdapter>(result);
-    }
+            Assert.NotNull(result);
+            Assert.IsType<SqlServerAdapter>(result);
+        }
 
-    [Fact]
-    public void CreateConnection_ServerTypeMariaDb_ReturnConnectionMySql()
-    {
-        var serverType = ServerType.MariaDb;
+        [Fact]
+        public void CreateConnection_ServerTypeMySql_ReturnConnectionMySql()
+        {
+            var serverType = ServerType.MySql;
 
-        var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
+            var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
 
-        Assert.NotNull(result);
-        Assert.IsType<MySqlAdapter>(result);
-    }
+            Assert.NotNull(result);
+            Assert.IsType<MySqlAdapter>(result);
+        }
 
-    [Fact]
-    public void CreateConnection_ServerTypePostgres_ReturnConnectionPostgres()
-    {
-        var serverType = ServerType.PostgreSQL;
+        [Fact]
+        public void CreateConnection_ServerTypeMariaDb_ReturnConnectionMySql()
+        {
+            var serverType = ServerType.MariaDb;
 
-        var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
+            var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
 
-        Assert.NotNull(result);
-        Assert.IsType<PostgreSqlAdapter>(result);
-    }
+            Assert.NotNull(result);
+            Assert.IsType<MySqlAdapter>(result);
+        }
 
-    [Fact]
-    public void CreateConnection_ServerTypeNotSupported_ReturnException()
-    {
-        var serverType = ServerType.Oracle;
+        [Fact]
+        public void CreateConnection_ServerTypePostgres_ReturnConnectionPostgres()
+        {
+            var serverType = ServerType.PostgreSQL;
 
-        Assert.Throws<NotSupportedException>(()=> _factory.CreateConnection(serverType, It.IsAny<IConfiguration>()));
+            var result = _factory.CreateConnection(serverType, It.IsAny<IConfiguration>());
+
+            Assert.NotNull(result);
+            Assert.IsType<PostgreSqlAdapter>(result);
+        }
+
+        [Fact]
+        public void CreateConnection_ServerTypeNotSupported_ReturnException()
+        {
+            var serverType = ServerType.Oracle;
+
+            Assert.Throws<NotSupportedException>(() => _factory.CreateConnection(serverType, It.IsAny<IConfiguration>()));
+        }
     }
 }
